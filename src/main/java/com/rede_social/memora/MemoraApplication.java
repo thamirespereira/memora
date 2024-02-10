6e7b1f0eef6cf8bd190e364a5c8a2ecbd7b357ad
thamirespereira <thamiresfrpereira@hotmail.com>
2024-02-10 12:20:40 -0300

    First commit

---
+++ src/main/java/com/rede_social/memora/MemoraApplication.java
@@ -0,0 +1,13 @@
+package com.rede_social.memora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoraApplication.class, args);
+	}
+
+}
